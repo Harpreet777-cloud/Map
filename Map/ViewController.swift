7491740002f7804801afac60aaffb28c9dad9d73
Harpreet Kaur <harpreet1998kaur31@gmail.com>
2021-01-25 15:36:13 +0530

    new files

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var Map: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var location = CLLocationCoordinate2DMake(20.5937,78.9629)
+        var span = MKCoordinateSpan(latitutdedeltalongitudeDelta 0.2, 0.2)
+        var region = MKCoordinateRegion
+        
+        
+        var annotation = MKPointAnnotation()
+       // annotation.coordinate = coordinate
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 20.5937, longitude: 78.9629)
+        annotation.title = "Pizza Hut"
+        annotation.subtitle = "You are here"
+        Map.addAnnotation(annotation)
+        
+        
+        
+        
     }
 
 
